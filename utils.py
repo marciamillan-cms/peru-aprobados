494eda2a917f3f5ceadb21941d1d4432e0383b81
marciamillan-cms <marcia.millan@cmspeople.com>
2026-08-11 23:00:33 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -141,8 +141,8 @@ html, body, [class*="css"] {
     position: fixed;
     top: 18px;
     z-index: 999;
-    height: 44px;
-    max-width: 130px;
+    height: 60px;
+    max-width: 140px;
     object-fit: contain;
     filter: drop-shadow(0 2px 6px rgba(0, 0, 0, 0.4));
 }
